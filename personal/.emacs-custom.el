abdfb09333f918faa4673ddcdb72b17d053caf81
Sami Airaksinen <your@email.com>
2012-09-17 17:53:09 +0300

    Final custom faces.

---
+++ personal/.emacs-custom.el
@@ -0,0 +1,32 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(case-fold-search t)
+ '(chm-view-delay 1.0)
+ '(term-default-bg-color "black")
+ '(term-default-fg-color "lawn green"))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(cursor ((t (:background "red"))))
+ '(erc-direct-msg-face ((t (:foreground "orange"))))
+ '(erc-input-face ((t (:foreground "green3"))))
+ '(erc-nick-default-face ((t (:foreground "orangered2" :underline t :weight semi-bold))))
+ '(erc-notice-face ((t (:foreground "dodgerblue" :weight semi-bold))))
+ '(font-lock-builtin-face ((((class color) (background dark)) (:foreground "green4"))))
+ '(font-lock-comment-face ((((class color) (background dark)) (:foreground "brown3"))))
+ '(font-lock-constant-face ((((class color) (background dark)) (:foreground "mediumpurple2"))))
+ '(font-lock-function-name-face ((((class color) (background dark)) (:foreground "green2"))))
+ '(font-lock-keyword-face ((((class color) (background dark)) (:foreground "dark cyan"))))
+ '(font-lock-string-face ((((class color) (background dark)) (:foreground "darkorange"))))
+ '(font-lock-variable-name-face ((((class color) (background dark)) (:foreground "goldenrod1"))))
+ '(menu ((t (:inverse-video t))))
+ '(mode-line ((t (:background "goldenrod" :foreground "#323232" :box (:line-width 1 :color "ivory3" :style released-button)))))
+ '(mode-line-buffer-id ((t nil)))
+ '(vhdl-font-lock-attribute-face ((((class color) (background dark)) (:foreground "tan2"))))
+ '(vhdl-font-lock-directive-face ((((class color) (background dark)) (:foreground "tan4")))))
