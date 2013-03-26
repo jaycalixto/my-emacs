dcea665b947a995184219baaddd8b2448f297ae5
Sami Airaksinen <samiaira@gmail.com>
2013-03-26 21:42:03 +0200

    Fixing emacs custom file resolution.

--- personal/.emacs-custom.el
+++ personal/.emacs-custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(canlock-password "4ce727964ab5d43ff3f962d824d92e80a6e9d99e")
+ '(custom-safe-themes (quote ("ebf6314f5c7c71034ffe2d67db489c6d868be3cf8cdba02e5a936808c5df75c8" "81805c86e126018f339211bb3f03e1c9eae30adfbe72832bd02f89ca0cbe5885" "843a82ff3b91bec5430f9acdd11de03fc0f7874b15c1b6fbb965116b4c7bf830" default)))
  '(case-fold-search t)
  '(chm-view-delay 1.0)
  '(term-default-bg-color "black")
