232b614ff41d31a8c058eec869e0a6860926c08b
Sami Airaksinen <samiaira@gmail.com>
2013-04-20 13:04:26 +0300

    Customized whitespace-mode faces to be more discrete.

--- personal/.emacs-custom.el
+++ personal/.emacs-custom.el
@@ -4,12 +4,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(canlock-password "4ce727964ab5d43ff3f962d824d92e80a6e9d99e")
- '(custom-safe-themes (quote ("ebf6314f5c7c71034ffe2d67db489c6d868be3cf8cdba02e5a936808c5df75c8" "81805c86e126018f339211bb3f03e1c9eae30adfbe72832bd02f89ca0cbe5885" "843a82ff3b91bec5430f9acdd11de03fc0f7874b15c1b6fbb965116b4c7bf830" default)))
  '(case-fold-search t)
  '(chm-view-delay 1.0)
- '(term-default-bg-color "black")
- '(term-default-fg-color "lawn green")
-)
+ '(custom-safe-themes (quote ("ebf6314f5c7c71034ffe2d67db489c6d868be3cf8cdba02e5a936808c5df75c8" "81805c86e126018f339211bb3f03e1c9eae30adfbe72832bd02f89ca0cbe5885" "843a82ff3b91bec5430f9acdd11de03fc0f7874b15c1b6fbb965116b4c7bf830" default)))
+ '(global-whitespace-mode t)
+ '(whitespace-line-column 120))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -23,4 +22,12 @@
  '(menu ((t (:inverse-video t))))
  '(mode-line ((t (:background "dark goldenrod" :foreground "midnight blue" :box (:line-width 3 :color "red" :style released-button)))))
  '(mode-line-buffer-id ((t nil)))
-)
+ '(whitespace-empty ((t (:foreground "dark red"))))
+ '(whitespace-indentation ((t (:foreground "dark red"))))
+ '(whitespace-line ((t (:background "gray5"))))
+ '(whitespace-newline ((t (:foreground "gray10" :weight normal))))
+ '(whitespace-space ((t (:foreground "gray10"))))
+ '(whitespace-space-after-tab ((t (:background "gray6" :foreground "dark red"))))
+ '(whitespace-space-before-tab ((t (:foreground "gray10"))))
+ '(whitespace-tab ((t (:foreground "gray10"))))
+ '(whitespace-trailing ((t (:foreground "dark red" :weight bold)))))
